de777800a2848fc7b63d0f47fadc5d1ee6c62dcb
Jeff Gore <jeff.gore@aexp.com>
2020-10-03 13:43:00 -0400

    chore: more HTTPS work

--- src/server/index.test.ts
+++ src/server/index.test.ts
@@ -1,9 +1,66 @@
-/*
-- test HTTP vs HTTPS use
-*/
+/* eslint-disable global-require, @typescript-eslint/unbound-method */
+import http, { Server } from 'http';
+import https from 'https';
+import app from './app';
 
-describe('hi', () => {
-  it('fails', () => {
-    throw new Error('write this test');
+jest.mock('./app');
+
+const httpServerMock = {} as http.Server;
+const httpsServerMock = {} as https.Server;
+
+httpServerMock.listen = jest.fn();
+httpsServerMock.listen = jest.fn();
+
+describe('Server', () => {
+  const httpSpy = jest
+    .spyOn(http, 'createServer')
+    .mockImplementation(jest.fn(() => httpServerMock));
+  const httpsSpy = jest
+    .spyOn(https, 'createServer')
+    .mockImplementation(jest.fn(() => httpsServerMock));
+
+  beforeEach(() => {
+    jest.clearAllMocks();
   });
+
+  describe('When the environment is not production', () => {
+    beforeEach(() => {
+      jest.isolateModules(() => {
+        require('.');
+      });
+    });
+
+    it('creates an HTTP server with the Express application', () => {
+      expect(httpSpy).toHaveBeenCalledWith(app);
+      expect(httpsSpy).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('When the environment is production', () => {
+    beforeEach(() => {
+      process.env.NODE_ENV = 'production';
+      jest.isolateModules(() => {
+        require('.');
+      });
+    });
+
+    it('creates an HTTPS server with the certificates and the Express application', () => {
+      expect(httpsSpy).toHaveBeenCalledWith(
+        expect.objectContaining({
+          key: expect.any(String),
+          cert: expect.any(String),
+        }),
+        app
+      );
+      expect(httpSpy).not.toHaveBeenCalled();
+    });
+  });
+
+  // describe('When process.env.LISTEN_PORT is defined', () => {
+
+  // })
+
+  // describe('When process.env.LISTEN_PORT is not defined', () => {
+
+  // })
 });
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,31 +1,40 @@
 import http from 'http';
 import https from 'https';
+import fs from 'fs';
+import path from 'path';
+import { Application } from 'express';
 
 import app from './app';
 
-export const isProductionMode = () => process.env.NODE_ENV === 'production';
-export const isDevelopmentMode = () => !isProductionMode();
+export const isProductionMode = (): boolean =>
+  process.env.NODE_ENV === 'production';
+export const isDevelopmentMode = (): boolean => !isProductionMode();
 
-function createServer(expressApp: Express.Application): http.Server {
-  const UTF8 = 'utf8';
-  const ASCII = 'ascii';
-
+function createServer(expressApp: Application): http.Server | https.Server {
   if (isDevelopmentMode()) {
-    return http.createServer(app);
+    return http.createServer(expressApp);
   }
 
-  return https.createServer(
-    // {
-    //   cert:
-    //   key:
-    //   passphrase:
-    // },
-    {},
-    app
-  );
+  const httpsOptions = {
+    key: fs.readFileSync(
+      path.join(__dirname, '../..', 'secrets/certs/key.pem'),
+      'utf-8'
+    ),
+    cert: fs.readFileSync(
+      path.join(__dirname, '../..', 'secrets/certs/cert.pem'),
+      'utf-8'
+    ),
+  };
+
+  return https.createServer(httpsOptions, expressApp);
 }
 
 const server = createServer(app);
+const port = process.env.LISTEN_PORT || 3000;
 
-// eslint-disable-next-line no-console
-server.listen(3000, () => console.log('Listening on port 3000'));
+server.listen(port, () =>
+  // eslint-disable-next-line no-console
+  console.log(
+    `${isProductionMode() ? 'HTTPS' : 'HTTP'} server listening on port ${port}`
+  )
+);
