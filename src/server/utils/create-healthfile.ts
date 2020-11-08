9044f8e54a051fbb7b77b0aa722cb789ca28cb3e
Jeff Gore <jeff.gore@aexp.com>
2020-11-08 10:46:52 -0500

    chore: another attempt fix healthfile

--- src/server/utils/create-healthfile.ts
+++ src/server/utils/create-healthfile.ts
@@ -9,27 +9,39 @@ const healthLocation = '../health.json';
 export default function createHealthfile(
   healthLocationOverride?: string
 ): void {
-  childProcess.exec(
-    'git rev-parse HEAD',
-    (err: childProcess.ExecException, stdout: string) => {
-      if (err) {
-        throw new Error(
-          `Error writing healthfile: could not get commit hash, error: ${err}`
-        );
-      } else {
-        const health = {
-          commit: stdout.replace('\n', ''),
-          version,
-        };
-        const relativePathToHealthfile =
-          healthLocationOverride || healthLocation;
-        fs.writeFileSync(
-          path.join(__dirname, relativePathToHealthfile),
-          JSON.stringify(health, null, 2)
-        );
+  const relativePathToHealthfile = healthLocationOverride || healthLocation;
+
+  if (process.env.SOURCE_VERSION) {
+    const health = {
+      commit: process.env.SOURCE_VERSION,
+      version,
+    };
+    fs.writeFileSync(
+      path.join(__dirname, relativePathToHealthfile),
+      JSON.stringify(health, null, 2)
+    );
+  } else {
+    childProcess.exec(
+      'git rev-parse HEAD',
+      (err: childProcess.ExecException, stdout: string) => {
+        if (err) {
+          throw new Error(
+            `Error writing healthfile: could not get commit hash, error: ${err}`
+          );
+        } else {
+          const health = {
+            commit: stdout.replace('\n', ''),
+            version,
+          };
+
+          fs.writeFileSync(
+            path.join(__dirname, relativePathToHealthfile),
+            JSON.stringify(health, null, 2)
+          );
+        }
       }
-    }
-  );
+    );
+  }
 }
 
 createHealthfile('../../../dist/server/health.json');
