aa273af9d5577f511e6dfcdb1a2bfe15e4ebeb01
Jeff Gore <jgore00@gmail.com>
2021-03-07 08:50:06 -0500

    chore: remove unneeded lint exceptions, justify existing ones

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,16 +24,12 @@ module.exports = {
   ],
   plugins: ['@typescript-eslint', 'jest'],
   rules: {
-    'react/prop-types': 0,
-    'react/jsx-filename-extension': 0,
-    'import/prefer-default-export': 0,
-    'import/extensions': 0,
-    'comma-dangle': 0, // for some reason this is not turned off by prettier extension
-    'implicit-arrow-linebreak': 0, // for some reason this is not turned off by prettier extension
-    'function-paren-newline': 0, // for some reason this is not turned off by prettier extension
-    'operator-linebreak': 0, // for some reason this is not turned off by prettier extension
+    'import/prefer-default-export': 0, // I disagree, named exports are easier to (1) find in codebase and (2) spy on in tests
+    'implicit-arrow-linebreak': 0, // ** for some reason this is not turned off by prettier extension **
+    'function-paren-newline': 0, // ** for some reason this is not turned off by prettier extension **
+    'operator-linebreak': 0, // ** for some reason this is not turned off by prettier extension **
     'object-curly-newline': 0, // complains about perfectly reasonable one-line destructuring
-    '@typescript-eslint/restrict-template-expressions': 0,
+    '@typescript-eslint/restrict-template-expressions': 0, // often used for error logs, in which the thrown object being interpolated into string is of unknown type
     'no-void': 0, // conflicts with typescript-eslint/no-floating-promises, which recommends using `void` for non-awaited Promises
   },
   globals: {
