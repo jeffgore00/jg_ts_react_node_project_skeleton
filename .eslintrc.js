6628fe98c4d4cebd3d947f9f1471008838006601
Jeff Gore <jgore00@gmail.com>
2021-04-25 21:51:41 -0400

    chore: make  changes to eslintrc after prettier major upgrade

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:jest/recommended',
-    'prettier/@typescript-eslint',
+    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -26,10 +26,6 @@ module.exports = {
   ],
   plugins: ['@typescript-eslint', 'jest'],
   rules: {
-    'implicit-arrow-linebreak': 0, // ** for some reason this is not turned off by prettier extension **
-    'function-paren-newline': 0, // ** for some reason this is not turned off by prettier extension **
-    'operator-linebreak': 0, // ** for some reason this is not turned off by prettier extension **
-    'object-curly-newline': 0, // complains about perfectly reasonable one-line destructuring
     'no-void': 0, // conflicts with @typescript-eslint/no-floating-promises, which recommends using `void` for non-awaited Promises
     '@typescript-eslint/restrict-template-expressions': 0, // often used for error logs, in which the thrown object being interpolated into string is of unknown type
     'import/prefer-default-export': 0, // I disagree, named exports are easier to (1) find in codebase and (2) spy on in tests,
